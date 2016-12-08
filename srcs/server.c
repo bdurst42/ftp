f07c4fe6405e05a86a70b78d1a6a2f78e25f6c19
Basile DURST <bdurst@e3r9p20.42.fr>
2016-12-08 21:24:55 +0100

    cd, gestion du path

--- srcs/server.c
+++ srcs/server.c
@@ -19,25 +19,88 @@ static int	ftp_create_server(int port)
 	return (sock);	
 }
 
-void			ftp_put_file(char **args, int c_sock)
+int				ftp_find_last_directory(char *path)
 {
-	int fd;
+	int	i;
 
-	if ((fd = open(ft_strtrim(args[1]), O_RDONLY)) == -1)
-	{
-		ftp_send_package("ERROR: We can't open this file", c_sock, 0);
-		ftp_error("NULL", "open failure\n");
-	}
-	else
-	{
-		
-	}
+	i = ft_strlen(path);
+	while (i > 0 && path[i] != '/')
+		--i;
+	return (i);
 }
 
-char            ftp_is_cmd(char *cmd, int c_sock)
+char			*ftp_check_path(char *old_path, char *path)
+{
+	int			i;
+	int			j;
+	int			pos;
+	char		*current_path;
+
+//	ft_putstr("getcwd = ");
+//	ft_putendl(getcwd(NULL, 0));
+//	ft_putstr("old_path = ");
+//	ft_putendl(old_path);
+//	ft_putstr("ft_strstr = ");
+//	ft_putendl(ft_strstr(getcwd(NULL, 0), old_path));
+	current_path = ft_strstr(getcwd(NULL, 0), old_path) + ft_strlen(old_path);
+//	ft_putstr("current_path = ");
+//	ft_putendl(current_path);
+//	if (!ft_strcmp(current_path, ""))
+//		current_path = "/";
+//	ft_putstr("path = ");
+//	ft_putendl(path);
+	if (path && path[0] != '/')
+		path = ft_strjoin("/", path);
+//	ft_putstr("path = ");
+//	ft_putendl(path);
+	i = -1;
+	while (path[++i])
+	{
+		if (path[i] == '/')
+			continue ;
+		j = i;
+//		ft_putstr("i = ");
+//		ft_putnbr(i);
+//		ft_putstr(" || j = ");
+//		ft_putnbr(j);
+//		ft_putstr("\n");
+		if (path[j] && path[j++] != '.')
+		{
+			while (path[j] && path[j] != '/')
+				++j;
+//			ft_putstr("directory i = ");
+//			ft_putnbr(i);
+//			ft_putstr(" || j = ");
+//			ft_putnbr(j);
+//			ft_putstr("\n");
+//			ft_putendl(ft_strsub(path, i, j - i));
+			current_path = ft_strjoin(current_path, ft_strsub(path, i - 1, j - i + 1));
+		}
+		else
+		{
+			while (path[j] == '.')
+				if (j++ != i)
+					if ((pos = ftp_find_last_directory(current_path)))
+						current_path = ft_strsub(current_path, 0, pos);
+		}
+		i = j;
+	}
+	current_path = ft_strjoin(old_path, current_path);
+//	ft_putendl(current_path);
+	return (current_path);
+}
+
+void			ftp_cd(char *path, int c_sock)
+{
+	if ((chdir(path)) == -1)
+		ftp_send_package("cd failure", c_sock, 0);
+}
+
+char            ftp_is_cmd(char *cmd, int c_sock, char *path)
 {
 	char	**args;
 
+	path = getcwd(NULL, 0);
 	args = ft_strsplit(cmd, ' ');
 	ft_putendl(cmd);
 	if (!ft_strcmp(cmd, "pwd"))
@@ -48,8 +111,11 @@ char            ftp_is_cmd(char *cmd, int c_sock)
 		if (execv("/bin/ls", args) == -1)
 			ftp_error("NULL", "execve failure\n");
 	}
-	else if (!ft_strncmp(cmd, "cd", 2))
+	else if (!ft_strncmp(cmd, "cd ", 3))
+	{
+		ftp_cd(ftp_check_path(path, ft_strtrim(cmd + 3)), c_sock);
 		ftp_send_package("SUCCES: cd", c_sock, 0);
+	}
 	else if (!ft_strncmp(cmd, "get ", 4))
 	{
 		ftp_send_file(cmd, c_sock);
@@ -75,13 +141,15 @@ void		ftp_fork(int c_sock)
 {
 	pid_t		pid;
 	t_header	header;
+	char		*path;
 
+	path = getcwd(NULL, 0);
 	if ((pid = fork()) == -1)
 		ftp_send_package("ERROR: fork failure !", c_sock, 0);
 	else if (pid)
 	{
 		ftp_send_package("SUCCES: connection", c_sock, 0);
-		while (ftp_is_cmd(ftp_get_package(c_sock, &header), c_sock))
+		while (ftp_is_cmd(ftp_get_package(c_sock, &header), c_sock, path))
 			;
 	}
 }
